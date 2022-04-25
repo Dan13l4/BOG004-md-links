8fbfd10b7e10065097ff6a887ec66c9d5286d2df
Dan13l4 <kdbaron426@gmail.com>
2022-04-25 09:02:00 -0500

    Creando bolerplate

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  // ...
+const utils = require("./src/links.js");
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    let arrayLinks = [];
+
+    if (!utils.existRoute(path)) {
+      reject("Route not exist")
+    }
+  })
 };
---
+++ src/links.js
@@ -0,0 +1,13 @@
+const mdLinks = (args) => {
+    const fs = require("fs");
+    const path = require('path');
+
+    const terminalPath = args[2];
+
+    const pathAbsolute =
+
+}
+
+utils.existRoute = existRoute;
+utils.convertPath = convertPath;
+module.exports = utils;
